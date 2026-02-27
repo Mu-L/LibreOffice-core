13eb92c5264c1cb2a96d5fdc9b96c062de9de177
Ujjawal Kumar <ujjawal.kumarchouhan@collabora.com>
2026-02-27 14:54:05 +0530

    ooxml: Save <picture> before control objs as listed in the ECMA specs
    Bug document: fdo69453-1.xls
    
    The above document contains <picture> tag on sheet 20 and 21 and is written
    after the <mc:AlternateContent> which is an incorrect order according to the
    ECMA specs and should be written before control objects.
    
    Change-Id: I14bb128281c75d47ee063dd516588a58a0f37dce
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200905
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201529
    (cherry picked from commit dc99252356908c7139f6365a999f437bd0d56bf8)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/204496
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/source/filter/excel/xeescher.cxx
+++ sc/source/filter/excel/xeescher.cxx
@@ -2033,7 +2033,7 @@ void XclExpObjectManager::StartSheet()
     mxObjList = new XclExpObjList( GetRoot(), *mxEscherEx );
 }
 
-rtl::Reference< XclExpRecordBase > XclExpObjectManager::ProcessDrawing( const SdrPage* pSdrPage )
+rtl::Reference< XclExpRecordBase > XclExpObjectManager::ProcessDrawing( const SdrPage* pSdrPage, std::unique_ptr<XclExpImgData> pImgData )
 {
     if( pSdrPage )
         mxEscherEx->AddSdrPage( *pSdrPage, GetOutput() != EXC_OUTPUT_BINARY );
@@ -2041,6 +2041,8 @@ rtl::Reference< XclExpRecordBase > XclExpObjectManager::ProcessDrawing( const Sd
     OSL_ENSURE( mxEscherEx->GetGroupLevel() <= 1, "XclExpObjectManager::ProcessDrawing - still groups open?" );
     while( mxEscherEx->GetGroupLevel() )
         mxEscherEx->LeaveGroup();
+    if (pImgData)
+        mxObjList->SetImageData(std::move(pImgData));
     mxObjList->EndSheet();
     return mxObjList;
 }
