LibreOffice/core

commit 3548c92453b9d0d85270bc6309a91c4107e49685

tdf#129810: Compress fullwidth CJK punctuation
When compressions CJK punctuation, compress also full width versions to
match Word behaviour.

Change-Id: Ic35cfcbacca1974b7241d657f078148bac06478e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153530
Tested-by: Jenkins
Reviewed-by: خالد حسني <khaled@libreoffice.org>
(cherry picked from commit 4a92323b54e7d63a8bc0b8e62fdc6b31760dcd05)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153519

editeng/source/editeng/impedit3.cxx
@@ -140,14 +140,16 @@ AsianCompressionFlags GetCharTypeForCompression( sal_Unicode cChar )
     {
         case 0x3008: case 0x300A: case 0x300C: case 0x300E:
         case 0x3010: case 0x3014: case 0x3016: case 0x3018:
-        case 0x301A: case 0x301D:
+        case 0x301A: case 0x301D: case 0xFF09: case 0xFF3D:
+        case 0xFF5D:
         {
             return AsianCompressionFlags::PunctuationRight;
         }
         case 0x3001: case 0x3002: case 0x3009: case 0x300B:
         case 0x300D: case 0x300F: case 0x3011: case 0x3015:
         case 0x3017: case 0x3019: case 0x301B: case 0x301E:
-        case 0x301F:
+        case 0x301F: case 0xFF08: case 0xFF0C: case 0xFF0E:
+        case 0xFF1A: case 0xFF1B: case 0xFF3B: case 0xFF5B:
         {
             return AsianCompressionFlags::PunctuationLeft;
         }

sw/qa/core/text/text.cxx
@@ -18,6 +18,7 @@
 #include <com/sun/star/text/XDocumentIndex.hpp>
 
 #include <vcl/gdimtf.hxx>
+#include <vcl/metaact.hxx>
 #include <vcl/filter/PDFiumLibrary.hxx>
 #include <comphelper/propertyvalue.hxx>
 #include <editeng/fhgtitem.hxx>
@@ -1360,6 +1361,38 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testParaUpperMarginFlyIntersect)
     // - Actual  : 857 (~1000)
     // I.e. both upper and lower margin was taken into account.
     CPPUNIT_ASSERT_EQUAL(521, nHeight);
+}
+
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf129810)
+{
+    // Load the document, which embeds a CJK font.
+    createSwDoc("tdf129810.odt");
+
+    // Render the document to a metafile.
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    SwDocShell* pShell = pTextDoc->GetDocShell();
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    CPPUNIT_ASSERT(xMetaFile);
+
+    // Find the fist text array action
+    for (size_t nAction = 0; nAction < xMetaFile->GetActionSize(); nAction++)
+    {
+        auto pAction = xMetaFile->GetAction(nAction);
+        if (pAction->GetType() == MetaActionType::TEXTARRAY)
+        {
+            auto pTextArrayAction = static_cast<MetaTextArrayAction*>(pAction);
+            auto pDXArray = pTextArrayAction->GetDXArray();
+
+            // There should be 13 chars on the first line
+            CPPUNIT_ASSERT_GREATER(size_t(13), pDXArray.size());
+
+            // Assert we are using the expected width for uncompressed chars
+            CPPUNIT_ASSERT_EQUAL(sal_Int32(720), pDXArray[0]);
+            // Assert we are using the expected width for compressed chars
+            CPPUNIT_ASSERT_EQUAL(sal_Int32(500), pDXArray[6] - pDXArray[5]);
+            break;
+        }
+    }
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/core/text/porlay.cxx
@@ -1491,6 +1491,7 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
                 case 0x3008: case 0x300A: case 0x300C: case 0x300E:
                 case 0x3010: case 0x3014: case 0x3016: case 0x3018:
                 case 0x301A: case 0x301D:
+                case 0xFF08: case 0xFF3B: case 0xFF5B:
                     eState = SPECIAL_LEFT;
                     break;
                 // Right punctuation found
@@ -1498,9 +1499,11 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
                 case 0x300D: case 0x300F: case 0x3011: case 0x3015:
                 case 0x3017: case 0x3019: case 0x301B: case 0x301E:
                 case 0x301F:
+                case 0xFF09: case 0xFF3D: case 0xFF5D:
                     eState = SPECIAL_RIGHT;
                     break;
                 case 0x3001: case 0x3002:   // Fullstop or comma
+                case 0xFF0C: case 0xFF0E: case 0xFF1A: case 0xFF1B:
                     eState = SPECIAL_MIDDLE ;
                     break;
                 default:

Mu-L/LibreOffice-core

commit b155465ef853cb61fa67e61c26fff27027850826

tdf#171262 Fix Full-width bracket mapping in GetCharTypeForCompression
When Asian layout compression is enabled, full-width brackets in text
boxes were overlapping due to incorrect character type assignment.

In the context of Asian typography compression:

    Opening brackets (e.g., U+FF08 '（') have space on their left side and
    occupy the right side of the character cell. Thus, they should be
    categorized as AsianCompressionFlags::PunctuationRight.
    Closing brackets (e.g., U+FF09 '）') have space on their right side and
    occupy the left side of the character cell. Thus, they should be
    categorized as AsianCompressionFlags::PunctuationLeft.

This commit swaps the following full-width characters to their correct
compression categories to prevent abnormal overlapping:

    Move U+FF08, U+FF3B, U+FF5B to PunctuationRight
    Move U+FF09, U+FF3D, U+FF5D to PunctuationLeft

Corrects the regression from 3548c92453b9d0d85270bc6309a91c4107e49685.

Change-Id: I3320b7d931eb3b399978198c7772a6edcf2b9725
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201608
Tested-by: Jenkins
Reviewed-by: Khaled Hosny <khaled@libreoffice.org>

editeng/source/editeng/impedit3.cxx
@@ -130,16 +130,16 @@ AsianCompressionFlags GetCharTypeForCompression( sal_Unicode cChar )
     {
         case 0x3008: case 0x300A: case 0x300C: case 0x300E:
         case 0x3010: case 0x3014: case 0x3016: case 0x3018:
-        case 0x301A: case 0x301D: case 0xFF09: case 0xFF3D:
-        case 0xFF5D:
+        case 0x301A: case 0x301D: case 0xFF08: case 0xFF3B:
+        case 0xFF5B:
         {
             return AsianCompressionFlags::PunctuationRight;
         }
         case 0x3001: case 0x3002: case 0x3009: case 0x300B:
         case 0x300D: case 0x300F: case 0x3011: case 0x3015:
         case 0x3017: case 0x3019: case 0x301B: case 0x301E:
-        case 0x301F: case 0xFF08: case 0xFF0C: case 0xFF0E:
-        case 0xFF1A: case 0xFF1B: case 0xFF3B: case 0xFF5B:
+        case 0x301F: case 0xFF09: case 0xFF0C: case 0xFF0E:
+        case 0xFF1A: case 0xFF1B: case 0xFF3D: case 0xFF5D:
         {
             return AsianCompressionFlags::PunctuationLeft;
         }