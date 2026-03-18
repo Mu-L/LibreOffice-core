LibreOffice/core

commit 2e0381693bce6a2ed16e72984edcb587174939fb

Fix out-of-bound crash in test dialog 2
It is visible in LOK case (only some backends).
TreeView requires width declaration for columns to not
fail on "you have only 2 columns" in GetTabs() call
we introduced recently.

Change-Id: Iae07f0813e962e413054794afc0228933af00dec
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187373
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194001
Tested-by: Jenkins
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

cui/source/dialogs/widgettestdlg.cxx
@@ -38,6 +38,17 @@ void WidgetTestDialog::FillTreeView()
     OUString aImage1(RID_SVXBMP_CELL_LR);
     OUString aImage2(RID_SVXBMP_SHADOW_BOT_LEFT);
 
+    // some backends might crash without proper widths set
+    std::vector<int> aWidths;
+    aWidths.push_back(m_xTreeView2->get_checkbox_column_width());
+    for (int i = 0; i < 3 - 1; ++i)
+    {
+        int nWidth = m_xTreeView2->get_column_width(i);
+        assert(nWidth > 0 && "suspicious to get a value like this");
+        aWidths.push_back(nWidth);
+    }
+    m_xTreeView2->set_column_fixed_widths(aWidths);
+
     for (size_t nCount = 0; nCount < 4; nCount++)
     {
         OUString sText = OUString::Concat("Test ") + OUString::Concat(OUString::number(nCount));
@@ -45,13 +56,13 @@ void WidgetTestDialog::FillTreeView()
         m_xTreeView->insert(nullptr, -1, &sText, &sText, nullptr, nullptr, false, xEntry.get());
         m_xTreeView->set_image(*xEntry, (nCount % 2 == 0) ? aImage1 : aImage2);
 
+        int nRow = m_xTreeView2->n_children();
         m_xTreeView2->append();
-        m_xTreeView2->set_image(nCount, (nCount % 2 == 0) ? aImage1 : aImage2);
-        m_xTreeView2->set_text(nCount, u"First Column"_ustr, 0);
-        // FIXME: crash out-of-bound
-        //m_xTreeView2->set_text(
-        //    nCount, OUString::Concat("Row ") + OUString::Concat(OUString::number(nCount)), 1);
-        m_xTreeView2->set_id(nCount, OUString::number(nCount));
+        m_xTreeView2->set_image(nRow, (nCount % 2 == 0) ? aImage1 : aImage2);
+        m_xTreeView2->set_text(nRow, u"First Column"_ustr, 0);
+        m_xTreeView2->set_text(
+            nRow, OUString::Concat("Row ") + OUString::Concat(OUString::number(nCount)), 1);
+        m_xTreeView2->set_id(nRow, OUString::number(nCount));
     }
 }
 

cui/uiconfig/ui/widgettestdialog.ui
@@ -45,6 +45,8 @@
   </object>
   <object class="GtkTreeStore" id="liststore3">
     <columns>
+      <!-- column-name icon -->
+      <column type="GdkPixbuf"/>
       <!-- column-name text -->
       <column type="gchararray"/>
       <!-- column-name text1 -->
@@ -643,6 +645,21 @@
                           <object class="GtkTreeSelection"/>
                         </child>
                         <child>
+                          <object class="GtkTreeViewColumn" id="treeviewcolumn0">
+                            <property name="resizable">True</property>
+                            <property name="spacing">6</property>
+                            <property name="sort-indicator">True</property>
+                            <property name="clickable">True</property>
+                            <property name="title" translatable="no">.</property>
+                            <child>
+                              <object class="GtkCellRendererPixbuf" id="cellrenderer0"/>
+                              <attributes>
+                                <attribute name="pixbuf">0</attribute>
+                              </attributes>
+                            </child>
+                          </object>
+                        </child>
+                        <child>
                           <object class="GtkTreeViewColumn" id="treeviewcolumn1">
                             <property name="resizable">True</property>
                             <property name="spacing">6</property>
@@ -652,7 +669,7 @@
                             <child>
                               <object class="GtkCellRendererText" id="cellrenderer1"/>
                               <attributes>
-                                <attribute name="text">0</attribute>
+                                <attribute name="text">1</attribute>
                               </attributes>
                             </child>
                           </object>
@@ -667,7 +684,7 @@
                             <child>
                               <object class="GtkCellRendererText" id="cellrenderer2"/>
                               <attributes>
-                                <attribute name="text">1</attribute>
+                                <attribute name="text">2</attribute>
                               </attributes>
                             </child>
                           </object>

Mu-L/LibreOffice-core

commit 793a077ec204031ce6742e653320a25ea692d503

cui: Drop assert that triggers for gtk3 in WidgetTestDialog::FillTreeView
This assert was introduced in

    commit 2e0381693bce6a2ed16e72984edcb587174939fb
    Date:   Fri Jul 4 08:11:19 2025 +0000

        Fix out-of-bound crash in test dialog 2

, but triggers for gtk3:

    soffice.bin: .../cui/source/dialogs/widgettestdlg.cxx:82: void WidgetTestDialog::FillTreeView(): Assertion `nWidth > 0 && "suspicious to get a value like this"' failed.

Remove it for now to make the dialog work again with gtk3.

Possibly the method only returns a useful value after layout
has happened for gtk3 - see also this commint in
GtkInstanceTreeView::get_column_width:

    virtual int get_column_width(int nColumn) const override
    {
        GtkTreeViewColumn* pColumn = GTK_TREE_VIEW_COLUMN(g_list_nth_data(m_pColumns, nColumn));
        assert(pColumn && "wrong count");
        int nWidth = gtk_tree_view_column_get_width(pColumn);
        // https://github.com/exaile/exaile/issues/580
        // after setting fixed_width on a column and requesting width before
        // gtk has a chance to do its layout of the column means that the width
        // request hasn't come into effect
        if (!nWidth)
            nWidth = gtk_tree_view_column_get_fixed_width(pColumn);
        return nWidth;
    }

To trigger the dialog:

* in "Tools" -> "Customize", set a keyboard shortcut for
  function/UNO command ".uno:WidgetTestDialog"
* close the dialog and activat the shortcut

Change-Id: I0e2c25636acad995bdd0b5c225c48079a13fcc89
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201938
Tested-by: Jenkins
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

cui/source/dialogs/widgettestdlg.cxx
@@ -79,7 +79,6 @@ void WidgetTestDialog::FillTreeView()
     for (int i = 0; i < 3 - 1; ++i)
     {
         int nWidth = m_xTreeView2->get_column_width(i);
-        assert(nWidth > 0 && "suspicious to get a value like this");
         aWidths.push_back(nWidth);
     }
     m_xTreeView2->set_column_fixed_widths(aWidths);