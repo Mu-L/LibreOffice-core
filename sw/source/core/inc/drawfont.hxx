LibreOffice/core

commit 45ec7bd76196dcc60b4c2db2f6f00623ecbaf5a4

tdf#168251 cui offapi xmloff sw glyph scaling: extend UNO/UX/ODF
Add new paragraph justification options "Minimum glyph scaling"
and "Maximum glyph scaling" to improve typography, like DTP software
do, allowing to typeset visually better paragraphs, especially narrow
columns without unacceptably big word spacing.

* Add spin boxes to Alignment in paragraph formatting dialog window;

* Store properties in paragraph model:

  css::style::ParagraphProperties::ParaPropScaleWidthMinimum

  css::style::ParagraphProperties::ParaPropScaleWidthMaximum

  Note: desired glyph scaling is already supported by ODF/UNO,
  see css::style::CharacterProperties::CharScaleWidth.

* Implement visual layout of minimum and maximum glyph scaling,
  limited by the available extra word spacing and letter spacing,
  too.

  Note: last line can exceed the paragraph width, because minimum
  glyph scaling, also minimum letter scaling of the last line
  haven't been applied there, yet.

* Add ODF import/export (loext:text-scale-maximum and
  loext:text-scale-minimum, where style:text-scale is for the
  desired text scaling).

* Add ODF unit test.

Note: hyphenated lines, lines with multiple portions haven't
been using custom glyph scaling, yet.

Note: resolution of glyph scaling is 1 percent, which will
avoid of overgeneration of font types using variable fonts.

Change-Id: I9b4576dde4303437b64e37202c47f888f94c3f67
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190534
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/inc/drawfont.hxx
@@ -75,6 +75,7 @@ class SW_DLLPUBLIC SwDrawTextInfo
     tools::Long m_nSpace = 0;
     tools::Long m_nKern = 0;
     tools::Long m_nLetterSpacing = 0;
+    sal_Int16 m_nScaleWidth = 100;
     TextFrameIndex m_nNumberOfBlanks = TextFrameIndex{ 0 };
     sal_uInt8 m_nCursorBidiLevel = 0;
     bool m_bBullet : 1;
@@ -548,6 +549,16 @@ public:
     tools::Long GetLetterSpacing() const
     {
         return m_nLetterSpacing;
+    }
+
+    void SetScaleWidth( tools::Long nNew )
+    {
+        m_nScaleWidth = nNew;
+    }
+
+    tools::Long GetScaleWidth() const
+    {
+        return m_nScaleWidth;
     }
 
     void SetSpace( tools::Long nNew )

sw/source/core/text/inftxt.cxx
@@ -783,6 +783,10 @@ void SwTextPaintInfo::DrawText_( const OUString &rText, const SwLinePortion &rPo
             if ( rPor.GetLetterSpacing() != 0 )
                 aDrawInf.SetLetterSpacing( rPor.GetLetterSpacing() / sal_Int32(nLength) );
 
+            // set custom glyph scaling (hyphenation hasn't been supported yet)
+            // Note: set 100 percent, too (to reset the setting of the previous line)
+            aDrawInf.SetScaleWidth( rPor.GetScaleWidth() );
+
             m_pFnt->DrawText_( aDrawInf );
         }
     }

sw/source/core/txtnode/swfont.cxx
@@ -1140,6 +1140,13 @@ void SwSubFont::DrawText_( SwDrawTextInfo &rInf, const bool bGrey )
     if( !pLastFont || pLastFont->GetOwner() != m_nFontCacheId )
         ChgFnt( rInf.GetShell(), rInf.GetOut() );
 
+    // change glyph scaling, if needed
+    if ( GetPropWidth() != rInf.GetScaleWidth() )
+    {
+        SetPropWidth( rInf.GetScaleWidth() );
+        ChgFnt( rInf.GetShell(), rInf.GetOut() );
+    }
+
     SwDigitModeModifier aDigitModeModifier(rInf.GetOut(), rInf.GetFont()->GetLanguage(),
                                            SwModule::get()->GetCTLTextNumerals());
 

cui/source/inc/paragrph.hxx
@@ -164,6 +164,11 @@ class SvxParaAlignTabPage : public SfxTabPage
     std::unique_ptr<weld::MetricSpinButton> m_xLetterSpacingMinimum;
     std::unique_ptr<weld::MetricSpinButton> m_xLetterSpacingMaximum;
 
+    /// glyph scaling
+    std::unique_ptr<weld::Label> m_xLabelGlyphScaling;
+    std::unique_ptr<weld::MetricSpinButton> m_xGlyphScalingMinimum;
+    std::unique_ptr<weld::MetricSpinButton> m_xGlyphScalingMaximum;
+
     DECL_LINK(AlignHdl_Impl, weld::Toggleable&, void);
     DECL_LINK(LastLineHdl_Impl, weld::ComboBox&, void);
     DECL_LINK(TextDirectionHdl_Impl, weld::ComboBox&, void);

cui/source/tabpages/paragrph.cxx
@@ -1290,6 +1290,9 @@ SvxParaAlignTabPage::SvxParaAlignTabPage(weld::Container* pPage, weld::DialogCon
     , m_xLabelLetterSpacing(m_xBuilder->weld_label(u"labelLetterSpacing"_ustr))
     , m_xLetterSpacingMinimum(m_xBuilder->weld_metric_spin_button(u"spin_LETTER_SPACING_MIN"_ustr, FieldUnit::PERCENT))
     , m_xLetterSpacingMaximum(m_xBuilder->weld_metric_spin_button(u"spin_LETTER_SPACING_MAX"_ustr, FieldUnit::PERCENT))
+    , m_xLabelGlyphScaling(m_xBuilder->weld_label(u"labelGlyphScaling"_ustr))
+    , m_xGlyphScalingMinimum(m_xBuilder->weld_metric_spin_button(u"spin_GLYPH_SCALING_MIN"_ustr, FieldUnit::PERCENT))
+    , m_xGlyphScalingMaximum(m_xBuilder->weld_metric_spin_button(u"spin_GLYPH_SCALING_MAX"_ustr, FieldUnit::PERCENT))
 {
     SetExchangeSupport();
 
@@ -1383,7 +1386,9 @@ bool SvxParaAlignTabPage::FillItemSet( SfxItemSet* rOutSet )
             m_xWordSpacingMinimum->get_value_changed_from_saved() ||
             m_xWordSpacingMaximum->get_value_changed_from_saved() ||
             m_xLetterSpacingMinimum->get_value_changed_from_saved() ||
-            m_xLetterSpacingMaximum->get_value_changed_from_saved();
+            m_xLetterSpacingMaximum->get_value_changed_from_saved() ||
+            m_xGlyphScalingMinimum->get_value_changed_from_saved() ||
+            m_xGlyphScalingMaximum->get_value_changed_from_saved();
     }
 
     sal_uInt16 _nWhich = GetWhich( SID_ATTR_PARA_ADJUST );
@@ -1408,6 +1413,8 @@ bool SvxParaAlignTabPage::FillItemSet( SfxItemSet* rOutSet )
         aAdj.SetPropWordSpacingMaximum( m_xWordSpacingMaximum->get_value(FieldUnit::PERCENT) );
         aAdj.SetPropLetterSpacingMinimum( m_xLetterSpacingMinimum->get_value(FieldUnit::PERCENT) );
         aAdj.SetPropLetterSpacingMaximum( m_xLetterSpacingMaximum->get_value(FieldUnit::PERCENT) );
+        aAdj.SetPropScaleWidthMinimum( m_xGlyphScalingMinimum->get_value(FieldUnit::PERCENT) );
+        aAdj.SetPropScaleWidthMaximum( m_xGlyphScalingMaximum->get_value(FieldUnit::PERCENT) );
         rOutSet->Put( aAdj );
         bModified = true;
     }
@@ -1500,6 +1507,11 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
             m_xLetterSpacingMinimum->set_sensitive(true);
             m_xLetterSpacingMinimum->set_value(rAdj.GetPropLetterSpacingMinimum(), FieldUnit::PERCENT);
             m_xLetterSpacingMaximum->set_value(rAdj.GetPropLetterSpacingMaximum(), FieldUnit::PERCENT);
+            // TODO add GlyphScaling (CharScaleWidth)
+            m_xGlyphScalingMaximum->set_sensitive(true);
+            m_xGlyphScalingMinimum->set_sensitive(true);
+            m_xGlyphScalingMinimum->set_value(rAdj.GetPropScaleWidthMinimum(), FieldUnit::PERCENT);
+            m_xGlyphScalingMaximum->set_value(rAdj.GetPropScaleWidthMaximum(), FieldUnit::PERCENT);
         }
         else
         {
@@ -1513,6 +1525,9 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
             m_xLabelLetterSpacing->set_sensitive(false);
             m_xLetterSpacingMinimum->set_sensitive(false);
             m_xLetterSpacingMaximum->set_sensitive(false);
+            m_xLabelGlyphScaling->set_sensitive(false);
+            m_xGlyphScalingMinimum->set_sensitive(false);
+            m_xGlyphScalingMaximum->set_sensitive(false);
         }
     }
     else
@@ -1531,6 +1546,9 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
         m_xLabelLetterSpacing->set_sensitive(false);
         m_xLetterSpacingMinimum->set_sensitive(false);
         m_xLetterSpacingMaximum->set_sensitive(false);
+        m_xLabelGlyphScaling->set_sensitive(false);
+        m_xGlyphScalingMinimum->set_sensitive(false);
+        m_xGlyphScalingMaximum->set_sensitive(false);
     }
     m_xLastLineLB->set_active(nLBSelect);
 
@@ -1593,6 +1611,8 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
     m_xWordSpacingMaximum->save_value();
     m_xLetterSpacingMinimum->save_value();
     m_xLetterSpacingMaximum->save_value();
+    m_xGlyphScalingMinimum->save_value();
+    m_xGlyphScalingMaximum->save_value();
 
     UpdateExample_Impl();
 }
@@ -1613,6 +1633,8 @@ void SvxParaAlignTabPage::ChangesApplied()
     m_xWordSpacingMaximum->save_value();
     m_xLetterSpacingMinimum->save_value();
     m_xLetterSpacingMaximum->save_value();
+    m_xGlyphScalingMinimum->save_value();
+    m_xGlyphScalingMaximum->save_value();
 }
 
 IMPL_LINK_NOARG(SvxParaAlignTabPage, AlignHdl_Impl, weld::Toggleable&, void)
@@ -1631,6 +1653,10 @@ IMPL_LINK_NOARG(SvxParaAlignTabPage, AlignHdl_Impl, weld::Toggleable&, void)
     // TODO visualize CharKerning with percentage
     m_xLetterSpacingMinimum->set_sensitive(bJustify);
     m_xLetterSpacingMaximum->set_sensitive(bJustify);
+    m_xLabelGlyphScaling->set_sensitive(bJustify);
+    // TODO visualize CharScaleWidth with percentage
+    m_xGlyphScalingMinimum->set_sensitive(bJustify);
+    m_xGlyphScalingMaximum->set_sensitive(bJustify);
 
     bool bLastLineIsBlock = m_xLastLineLB->get_active() == 2;
     m_xExpandCB->set_sensitive(bJustify && bLastLineIsBlock);

cui/uiconfig/ui/paragalignpage.ui
@@ -34,6 +34,20 @@
     <property name="step-increment">1</property>
     <property name="page-increment">10</property>
   </object>
+  <object class="GtkAdjustment" id="adjustmentPercent6">
+    <property name="upper">100</property>
+    <property name="lower">50</property>
+    <property name="value">100</property>
+    <property name="step-increment">1</property>
+    <property name="page-increment">10</property>
+  </object>
+  <object class="GtkAdjustment" id="adjustmentPercent7">
+    <property name="upper">200</property>
+    <property name="lower">100</property>
+    <property name="value">100</property>
+    <property name="step-increment">1</property>
+    <property name="page-increment">10</property>
+  </object>
   <!-- n-columns=2 n-rows=1 -->
   <object class="GtkGrid" id="ParaAlignPage">
     <property name="visible">True</property>
@@ -594,6 +608,56 @@
                     <property name="top-attach">2</property>
                   </packing>
                 </child>
+                <child>
+                  <object class="GtkLabel" id="labelGlyphScaling">
+                    <property name="visible">True</property>
+                    <property name="can-focus">False</property>
+                    <property name="label" translatable="yes" context="paragalignpage|labelGlyphScaling">_Glyph scaling:</property>
+                    <property name="use-underline">True</property>
+                    <property name="xalign">0</property>
+                  </object>
+                  <packing>
+                    <property name="left-attach">0</property>
+                    <property name="top-attach">3</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkSpinButton" id="spin_GLYPH_SCALING_MIN">
+                    <property name="visible">True</property>
+                    <property name="can-focus">True</property>
+                    <property name="activates-default">True</property>
+                    <property name="truncate-multiline">True</property>
+                    <property name="adjustment">adjustmentPercent6</property>
+                    <child internal-child="accessible">
+                      <object class="AtkObject" id="spin_GLYPH_SCALING_MIN-atkobject">
+                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|GLYPH-SCALING-MIN">Adjusts the minimum glyph scaling. Enter a number between 50% (ultra-condensed) and 100% (regular).</property>
+                      </object>
+                    </child>
+                  </object>
+                  <packing>
+                    <property name="left-attach">1</property>
+                    <property name="top-attach">3</property>
+                  </packing>
+                </child>
+                <child>
+                  <object class="GtkSpinButton" id="spin_GLYPH_SCALING_MAX">
+                    <property name="visible">True</property>
+                    <property name="can-focus">True</property>
+                    <property name="activates-default">True</property>
+                    <property name="truncate-multiline">True</property>
+                    <property name="adjustment">adjustmentPercent7</property>
+                    <child internal-child="accessible">
+                      <object class="AtkObject" id="spin_GLYPH_SCALING_MAX-atkobject">
+                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|GLYPH-SCALING-MMAX">Adjusts the maximum glyph scaling. Enter a number between 100% (regular) and 200% (ultra-expanded).</property>
+                      </object>
+                    </child>
+                  </object>
+                  <packing>
+                    <property name="left-attach">3</property>
+                    <property name="top-attach">3</property>
+                  </packing>
+                </child>
+
                 <child>
                   <placeholder/>
                 </child>

editeng/source/items/paraitem.cxx
@@ -334,7 +334,9 @@ SvxAdjustItem::SvxAdjustItem(const SvxAdjust eAdjst, const sal_uInt16 nId )
     nPropWordSpacingMinimum(100),
     nPropWordSpacingMaximum(100),
     nPropLetterSpacingMinimum(0),
-    nPropLetterSpacingMaximum(0)
+    nPropLetterSpacingMaximum(0),
+    nPropScaleWidthMinimum(100),
+    nPropScaleWidthMaximum(100)
 {
     SetAdjust( eAdjst );
 }
@@ -352,7 +354,9 @@ bool SvxAdjustItem::operator==( const SfxPoolItem& rAttr ) const
            nPropWordSpacingMaximum == rItem.nPropWordSpacingMaximum &&
            nPropWordSpacing == rItem.nPropWordSpacing &&
            nPropLetterSpacingMinimum == rItem.nPropLetterSpacingMinimum &&
-           nPropLetterSpacingMaximum == rItem.nPropLetterSpacingMaximum;
+           nPropLetterSpacingMaximum == rItem.nPropLetterSpacingMaximum &&
+           nPropScaleWidthMinimum == rItem.nPropScaleWidthMinimum &&
+           nPropScaleWidthMaximum == rItem.nPropScaleWidthMaximum;
 }
 
 size_t SvxAdjustItem::hashCode() const
@@ -367,6 +371,8 @@ size_t SvxAdjustItem::hashCode() const
     o3tl::hash_combine(seed, nPropWordSpacingMaximum);
     o3tl::hash_combine(seed, nPropLetterSpacingMinimum);
     o3tl::hash_combine(seed, nPropLetterSpacingMaximum);
+    o3tl::hash_combine(seed, nPropScaleWidthMinimum);
+    o3tl::hash_combine(seed, nPropScaleWidthMaximum);
     return seed;
 }
 
@@ -382,6 +388,8 @@ bool SvxAdjustItem::QueryValue( uno::Any& rVal, sal_uInt8 nMemberId ) const
         case MID_WORD_SPACING_MAX : rVal <<= static_cast<sal_Int16>(GetPropWordSpacingMaximum()); break;
         case MID_LETTER_SPACING_MIN : rVal <<= GetPropLetterSpacingMinimum(); break;
         case MID_LETTER_SPACING_MAX : rVal <<= GetPropLetterSpacingMaximum(); break;
+        case MID_SCALE_WIDTH_MIN : rVal <<= GetPropScaleWidthMinimum(); break;
+        case MID_SCALE_WIDTH_MAX : rVal <<= GetPropScaleWidthMaximum(); break;
         case MID_EXPAND_SINGLE    :
         {
             rVal <<= bOneBlock;
@@ -448,6 +456,20 @@ bool SvxAdjustItem::PutValue( const uno::Any& rVal, sal_uInt8 nMemberId )
             sal_Int16 nVal = -1;
             rVal >>= nVal;
             SetPropLetterSpacingMaximum(nVal);
+        }
+        break;
+        case MID_SCALE_WIDTH_MIN :
+        {
+            sal_Int16 nVal = -1;
+            rVal >>= nVal;
+            SetPropScaleWidthMinimum(nVal);
+        }
+        break;
+        case MID_SCALE_WIDTH_MAX :
+        {
+            sal_Int16 nVal = -1;
+            rVal >>= nVal;
+            SetPropScaleWidthMaximum(nVal);
         }
         break;
         case MID_EXPAND_SINGLE :

include/editeng/adjustitem.hxx
@@ -55,6 +55,10 @@ class EDITENG_DLLPUBLIC SvxAdjustItem final : public SfxPoolItem
     sal_Int16 nPropLetterSpacingMinimum;
     sal_Int16 nPropLetterSpacingMaximum;
 
+    // minimum and maximum glyph scaling values in percent
+    sal_Int16 nPropScaleWidthMinimum;
+    sal_Int16 nPropScaleWidthMaximum;
+
 protected:
     virtual ItemInstanceManager* getItemInstanceManager() const override;
 
@@ -206,6 +210,27 @@ public:
     void SetPropLetterSpacingMaximum( sal_Int16 nVal )
     {
         nPropLetterSpacingMaximum = nVal;
+    }
+
+
+    sal_Int16 GetPropScaleWidthMaximum() const
+    {
+        return nPropScaleWidthMaximum;
+    }
+
+    void SetPropScaleWidthMaximum( sal_Int16 nVal )
+    {
+        nPropScaleWidthMaximum = nVal;
+    }
+
+    sal_Int16 GetPropScaleWidthMinimum() const
+    {
+        return nPropScaleWidthMinimum;
+    }
+
+    void SetPropScaleWidthMinimum( sal_Int16 nVal )
+    {
+        nPropScaleWidthMinimum = nVal;
     }
 };
 

Mu-L/LibreOffice-core

commit f25371fa95b2c46ff4a73ef6995c341c157e62d4

tdf#169491 tdf#168251 sw glyph scaling: fix all scale width regressions
Do not disable scale width character setting, if it is applied in a text
span (except when the paragraph uses automatic glyph scaling, yet).

Regression since commit 45ec7bd76196dcc60b4c2db2f6f00623ecbaf5a4
"tdf#168251 cui offapi xmloff sw glyph scaling: extend UNO/UX/ODF".

Follow-up to commit 7ed8a8ee39951f74e364091fafff20269359ba15
"tdf#171161 tdf#168251 sw glyph scaling: fix scale width regression".

Change-Id: Ic42e58d1b96d9697eb0ddd0d7b999171fc0f39eb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201946
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/inc/drawfont.hxx
@@ -75,7 +75,7 @@ class SW_DLLPUBLIC SwDrawTextInfo
     tools::Long m_nSpace = 0;
     tools::Long m_nKern = 0;
     tools::Long m_nLetterSpacing = 0;
-    sal_Int16 m_nScaleWidth = 100;
+    sal_Int16 m_nScaleWidth = -1;
     TextFrameIndex m_nNumberOfBlanks = TextFrameIndex{ 0 };
     sal_uInt8 m_nCursorBidiLevel = 0;
     bool m_bBullet : 1;

sw/source/core/text/inftxt.cxx
@@ -822,9 +822,6 @@ void SwTextPaintInfo::DrawText_( const OUString &rText, const SwLinePortion &rPo
                 // Note: set 100 percent, too (to reset the automatic scaling of the previous line)
                 aDrawInf.SetScaleWidth( GetScaleWidth() );
             }
-            else
-               // tdf#171161 otherwise use the user-defined scale width value
-               aDrawInf.SetScaleWidth( rAttrSet.GetCharScaleW().GetValue()  );
 
             m_pFnt->DrawText_( aDrawInf );
         }

sw/source/core/txtnode/swfont.cxx
@@ -1149,7 +1149,7 @@ void SwSubFont::DrawText_( SwDrawTextInfo &rInf, const bool bGrey )
         ChgFnt( rInf.GetShell(), rInf.GetOut() );
 
     // change glyph scaling, if needed
-    if ( GetPropWidth() != rInf.GetScaleWidth() )
+    if ( rInf.GetScaleWidth() > 0 && GetPropWidth() != rInf.GetScaleWidth() )
     {
         SetPropWidth( rInf.GetScaleWidth() );
         ChgFnt( rInf.GetShell(), rInf.GetOut() );