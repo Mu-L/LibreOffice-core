Mu-L/LibreOffice-core

commit 5772d9843048d6e32e28a8de66a320d0f41aefc6

Fraction->double in PictReader
Change-Id: I9cb7e5fbd36616afc7fd1dab101baa9c9e5d78e0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202598
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/ipict/ipict.cxx
@@ -175,8 +175,8 @@ private:
     Size          aActOvalSize;
     vcl::Font     aActFont;
 
-    Fraction        aHRes;
-    Fraction        aVRes;
+    double        mfHRes;
+    double        mfVRes;
 
     Point ReadPoint();
 
@@ -1259,8 +1259,8 @@ void PictReader::ReadHeader()
             fHRes /= 65536;
             double fVRes = nVResFixed;
             fVRes /= 65536;
-            aHRes /= fHRes;
-            aVRes /= fVRes;
+            fHRes /= fHRes;
+            fVRes /= fVRes;
             aBoundingRect=tools::Rectangle( x1,y1, x2, y2 );
             pPict->SeekRel( 4 ); // 4 bytes reserved
             return;
@@ -1947,7 +1947,7 @@ void PictReader::ReadPict( SvStream & rStreamPict, GDIMetaFile & rGDIMetaFile )
     aActFont.SetFontSize(Size(0,12));
     aActFont.SetAlignment(ALIGN_BASELINE);
 
-    aHRes = aVRes = Fraction( 1, 1 );
+    mfHRes = mfVRes = 1.0;
 
     pVirDev = VclPtr<VirtualDevice>::Create();
     pVirDev->EnableOutput(false);
@@ -2007,7 +2007,7 @@ void PictReader::ReadPict( SvStream & rStreamPict, GDIMetaFile & rGDIMetaFile )
     rGDIMetaFile.Stop();
     pVirDev.disposeAndClear();
 
-    rGDIMetaFile.SetPrefMapMode( MapMode( MapUnit::MapInch, Point(), aHRes, aVRes ) );
+    rGDIMetaFile.SetPrefMapMode( MapMode( MapUnit::MapInch, Point(), Fraction(mfHRes), Fraction(mfVRes) ) );
     rGDIMetaFile.SetPrefSize( aBoundingRect.GetSize() );
 
     pPict->SetEndian(nOrigNumberFormat);

commit 1279168289a8fa5e3a24ab7eaaa5753c277a13ab

fix divide-by-zero in PictReader
regression from commit
  5772d9843048d6e32e28a8de66a320d0f41aefc6
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Tue Mar 24 15:07:00 2026 +0200
  Fraction->double in PictReader

Change-Id: I3c082b7441828c6290854a63a9c9f2384173d6bc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202651
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/ipict/ipict.cxx
@@ -1259,8 +1259,10 @@ void PictReader::ReadHeader()
             fHRes /= 65536;
             double fVRes = nVResFixed;
             fVRes /= 65536;
-            fHRes /= fHRes;
-            fVRes /= fVRes;
+            if (fHRes != 0)
+                mfHRes /= fHRes;
+            if (fVRes != 0)
+                mfVRes /= fVRes;
             aBoundingRect=tools::Rectangle( x1,y1, x2, y2 );
             pPict->SeekRel( 4 ); // 4 bytes reserved
             return;