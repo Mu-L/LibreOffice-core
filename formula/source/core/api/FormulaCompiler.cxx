af113687101e63fe892f3e7f6983aa2213be0dda
Karthik <karthik.godha@collabora.com>
2026-04-10 17:51:45 +0530

    sc: Use "#REF!" for invalid `ocPush` tokens
    Invalid ocPush tokens are not handled in the FormulaCompiler
    
    bug-document: forum-mso-de-103430.xlsx
    Change-Id: I1e0cb7d5db7f0050c7d553ba30049737015f666b
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/747
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    (cherry picked from commit 0a8c627e725fb4c9666aa7307db15a5515958485)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/204494
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

--- formula/source/core/api/FormulaCompiler.cxx
+++ formula/source/core/api/FormulaCompiler.cxx
@@ -2573,7 +2573,7 @@ void FormulaCompiler::CreateStringFromTokenArray( OUStringBuffer& rBuffer )
                 }
             }
         }
-        if (t->GetOpCode() == ocExternal && t->GetType() == svByte)
+        if ((t->GetOpCode() == ocExternal || t->GetOpCode() == ocPush) && t->GetType() == svByte)
         {
             rBuffer.append(GetNativeSymbol(ocErrRef));
             t = maArrIterator.Next();
