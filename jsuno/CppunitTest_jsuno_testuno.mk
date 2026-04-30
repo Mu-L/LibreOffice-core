LibreOffice/core

QuickJS-based JS UNO support
Somewhat loosely based on <https://github.com/allotropia/zetajs>.

Main part that is still missing from this WIP commit is implementing UNO objects
in JS.  (And there are a number of TODOs throughout, mostly regarding error
handling.)

Change-Id: I8810e43fe5f3e4b1a53a654070fe87e95f51231a
Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/190
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/1171
Tested-by: Andras Timar <andras.timar@collabora.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>

engine/jsuno/CppunitTest_jsuno_testuno.mk
@@ -1,0 +1,29 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the Collabora Office project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,jsuno_testuno))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,jsuno_testuno, \
+    jsuno/qa/unit/testuno \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,jsuno_testuno, \
+    jsuno \
+    sal \
+))
+
+$(eval $(call gb_CppunitTest_use_components,jsuno_testuno, \
+    unotest/source/testuno/testuno \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,jsuno_testuno))
+
+$(eval $(call gb_CppunitTest_use_ure,jsuno_testuno))
+
+# vim: set noet sw=4 ts=4:

Mu-L/LibreOffice-core

===
Comments from Neil Roberts:
This patch was cherry-picked from
1cd8ad398b38763cb3578f1305a28e7774163714 in Collabora’s repo. The
following changes were made in order to get it to pass the CI:
* The unit test has been modified to continue using embindtest instead
  of testuno.
* The name of the external library is quickjs instead of qjs because a
  different patch is used to add the external QuickJS package.
* The asserts on argc that were captured in the lambda function have been
  moved outside of the lambda function in order to avoid having an unused
  capture on release builds. The argc parameter has then been marked as
  [[maybe_unused]].
* The constructors for JSClassDef have been changed to consistently use
  { } brace initialisers because otherwise it fails to build on macOS.
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/203963
Tested-by: Jenkins
Reviewed-by: Neil Roberts <nroberts@libreoffice.org>

jsuno/CppunitTest_jsuno_testuno.mk
@@ -1,0 +1,29 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the Collabora Office project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_CppunitTest_CppunitTest,jsuno_testuno))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,jsuno_testuno, \
+    jsuno/qa/unit/testuno \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,jsuno_testuno, \
+    jsuno \
+    sal \
+))
+
+$(eval $(call gb_CppunitTest_use_components,jsuno_testuno, \
+   unotest/source/embindtest/embindtest \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,jsuno_testuno))
+
+$(eval $(call gb_CppunitTest_use_ure,jsuno_testuno))
+
+# vim: set noet sw=4 ts=4: